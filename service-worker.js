a9b77971529b29d44345ee4a7d82b5cae56d7c6b
ykr71 <yk_701r.bts@ezweb.ne.jp>
2025-04-06 09:23:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,14 @@
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(['index.html', 'manifest.json', 'icon.png']);
     })
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
